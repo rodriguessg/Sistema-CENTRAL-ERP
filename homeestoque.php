5647b03dc279adfd7936c86dbcfd0792e1820cb7
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-22 16:38:00 -0300

    .
    .

--- homeestoque.php
+++ homeestoque.php
@@ -9,10 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Almoxarifado</title>
-     <link rel="stylesheet" href="src/style/modal.css">
     <link rel="stylesheet" href="src/estoque/style/estoque-conteudo.css">
 <!-- Font Awesome CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 
 
 </head>

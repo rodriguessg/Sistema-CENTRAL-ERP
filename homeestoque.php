1f6d2514b9ba8ba0fee324a278289d9d20501521
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-22 12:39:53 -0300

    .
    .

--- homeestoque.php
+++ homeestoque.php
@@ -10,8 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Almoxarifado</title>
      <link rel="stylesheet" href="src/style/modal.css">
-    <link rel="stylesheet" href="src/estoque/style/container-estoque.css">
-    <link rel="stylesheet" href="src/estoque/style/modal-sucesso.css">
+    <link rel="stylesheet" href="src/estoque/style/estoque-conteudo.css">
 <!-- Font Awesome CDN -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
